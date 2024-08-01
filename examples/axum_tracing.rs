3148bdb62ce816ce004fa62f1f899031a91d88ec
maya <770257551@qq.com>
2024-08-01 14:21:51 +0800

    features: axum tracing log with jaeger

--- examples/axum_tracing.rs
+++ examples/axum_tracing.rs
@@ -1,5 +1,19 @@
-use axum::{routing::get, Router};
-use tokio::{net::TcpListener, time::Instant};
+use std::time::Duration;
+
+use anyhow::Ok;
+use axum::{extract::Request, routing::get, Router};
+use opentelemetry::{trace::TracerProvider, KeyValue};
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{
+    runtime,
+    trace::{Config, RandomIdGenerator, Tracer},
+    Resource,
+};
+use tokio::{
+    join,
+    net::TcpListener,
+    time::{sleep, Instant},
+};
 use tracing::{debug, info, instrument, level_filters::LevelFilter, warn};
 use tracing_subscriber::{
     fmt::{self, format::FmtSpan},
@@ -10,22 +24,28 @@ use tracing_subscriber::{
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let file_appender = tracing_appender::rolling::hourly("/tmp/logs", "ecosystem.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-
+    //console layer
     let console = fmt::Layer::new()
         .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
         .pretty()
         .with_filter(LevelFilter::DEBUG);
 
+    //file appender layer
+    let file_appender = tracing_appender::rolling::hourly("/tmp/logs", "ecosystem.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
     let file = fmt::Layer::new()
         .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
         .with_writer(non_blocking)
         .with_filter(LevelFilter::INFO);
 
+    // opentelemetry tracing layer for tracing-subscriber
+    let tracer = init_tracer()?;
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
     tracing_subscriber::registry()
         .with(console)
         .with(file)
+        .with(opentelemetry)
         .init();
 
     let addr = "0.0.0.0:8080";
@@ -39,20 +59,64 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-#[instrument]
-async fn index_handler() -> &'static str {
+#[instrument(fields(http.uri = req.uri().path(), http.method = req.method().as_str()))]
+async fn index_handler(req: Request) -> &'static str {
     debug!("Index handler started");
     tokio::time::sleep(std::time::Duration::from_millis(10)).await;
     let ret = long_task().await;
-    info!(http.status = "200", "Index handler completed");
+    info!(http.status_code = "200", "Index handler completed");
     ret
 }
 
 #[instrument]
 async fn long_task() -> &'static str {
     let start = Instant::now();
-    tokio::time::sleep(std::time::Duration::from_millis(123)).await;
+    let sl = sleep(Duration::from_millis(11));
+    // spawn multiple tasks
+
+    let t1 = task1();
+    let t2 = task2();
+    let t3 = task3();
+    join!(sl, t1, t2, t3);
     let elapsed = start.elapsed().as_millis() as u64;
     warn!(app.task_duration = elapsed, "task takes too long");
-    "Hello world!"
+    "Hello, World!"
+}
+
+#[instrument]
+async fn task1() {
+    sleep(Duration::from_millis(10)).await;
+}
+
+#[instrument]
+async fn task2() {
+    sleep(Duration::from_millis(50)).await;
+}
+
+#[instrument]
+async fn task3() {
+    sleep(Duration::from_millis(30)).await;
+}
+
+fn init_tracer() -> anyhow::Result<Tracer> {
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint("http://localhost:4317"),
+        )
+        .with_trace_config(
+            Config::default()
+                .with_id_generator(RandomIdGenerator::default())
+                .with_max_events_per_span(32)
+                .with_max_attributes_per_span(64)
+                .with_resource(Resource::new(vec![KeyValue::new(
+                    "service.name",
+                    "my-basic-oltp-service",
+                )])),
+        )
+        .install_batch(runtime::Tokio)?;
+    let tracer = tracer.tracer("my_trace");
+    Ok(tracer)
 }
