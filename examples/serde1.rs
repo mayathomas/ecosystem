c5282b05987611ce1c3cbf072c8b1827f07d9ad6
maya <770257551@qq.com>
2024-08-02 11:07:31 +0800

    feature: serde with

--- examples/serde1.rs
+++ examples/serde1.rs
@@ -3,12 +3,14 @@ use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine};
 use chacha20poly1305::{aead::Aead, ChaCha20Poly1305, KeyInit};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
 
 //32 bytes
 const KEY: &[u8] = b"01234567890123456789012345678901";
 //12 bytes
 const NONCE: &[u8] = b"012345678901";
 
+#[serde_as]
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct User {
@@ -16,6 +18,7 @@ struct User {
     #[serde(rename = "myAge")]
     age: u8,
     date_of_birth: DateTime<Utc>,
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
     skils: Vec<String>,
     state: WorkState,
     #[serde(serialize_with = "b64_encode", deserialize_with = "b64_decode")]
@@ -25,6 +28,8 @@ struct User {
         deserialize_with = "deserialize_decrypt"
     )]
     sensitive: String,
+    #[serde_as(as = "Vec<DisplayFromStr>")]
+    url: Vec<http::Uri>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -96,6 +101,7 @@ fn main() -> Result<()> {
         state: state1,
         data: vec![1, 2, 3, 4, 5],
         sensitive: "secret".to_string(),
+        url: vec!["http://www.example.com".parse()?],
     };
 
     let json = serde_json::to_string(&user)?;
@@ -103,6 +109,7 @@ fn main() -> Result<()> {
 
     let user: User = serde_json::from_str(&json)?;
     println!("{:?}", user);
+    println!("{:?}", user.url[0].host());
 
     Ok(())
 }
